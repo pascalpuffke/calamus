b1672c4376533764d6851462925042b11c4c3cee
pascalpuffke <pascal@pascalpuffke.de>
2023-06-01 20:20:06 +0200

    Build: Update packages
    - fmt 9.1.0 => 10.0.0
    - raylib 4.2.0 => 4.5.0

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -6,10 +6,10 @@ if (NOT (EXISTS ${CPM_DOWNLOAD_LOCATION}))
 endif ()
 include(${CPM_DOWNLOAD_LOCATION})
 
-CPMAddPackage(NAME fmt VERSION 9.1.0 GITHUB_REPOSITORY fmtlib/fmt GIT_TAG 9.1.0)
+CPMAddPackage(NAME fmt VERSION 10.0.0 GITHUB_REPOSITORY fmtlib/fmt GIT_TAG 10.0.0)
 CPMAddPackage(NAME cxxopts VERSION 3.1.1 GITHUB_REPOSITORY jarro2783/cxxopts GIT_TAG v3.1.1)
 CPMAddPackage(NAME tomlplusplus VERSION 3.3.0 GITHUB_REPOSITORY marzer/tomlplusplus GIT_TAG v3.3.0)
-CPMAddPackage(NAME raylib VERSION 4.2.0 GITHUB_REPOSITORY raysan5/raylib GIT_TAG 4.2.0)
+CPMAddPackage(NAME raylib VERSION 4.5.0 GITHUB_REPOSITORY raysan5/raylib GIT_TAG 4.5.0)
 CPMAddPackage(NAME googletest VERSION 1.13.0 GITHUB_REPOSITORY google/googletest GIT_TAG v1.13.0)
 
 # Include the CPM generated files. There probably is a better way to do this.
