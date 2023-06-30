a30d5c791c3f77996aba573724832d9a3408fb3e
pascalpuffke <pascal@pascalpuffke.de>
2023-06-30 21:17:37 +0200

    Renderer: Add virtual destructor for `RenderLayer`
    So CLion doesn't get mad at me anymore

--- include/graphics/render_layer.h
+++ include/graphics/render_layer.h
@@ -8,6 +8,8 @@ class RenderLayer {
 public:
     explicit RenderLayer(Renderer& renderer)
         : renderer(renderer) { }
+    virtual ~RenderLayer() = default;
+
     virtual void on_render() = 0;
 
 protected:
